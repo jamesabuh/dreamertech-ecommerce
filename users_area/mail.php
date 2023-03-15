03a69b753991e10f97ef4afe40f388c29077f3ed
jamesabuh <jamesabuh25@gmail.com>
2023-03-15 15:16:11 +0100

    mail setup

--- users_area/mail.php
+++ users_area/mail.php
@@ -1,7 +1,7 @@
 <?php
     
     use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+    
 
     require 'PHPMailer-master/src/Exception.php';
     require 'PHPMailer-master/src/PHPMailer.php';
